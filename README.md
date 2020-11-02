1aeac1ebe7ee855e1a36aee46f05eab439d4ac0d
gergokoncz <gergokoncz06@gmail.com>
2020-11-02 14:28:54 +0100

    created sdcObject

--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 
 The project contains the anonymization of a dataset for proper disclosure in order to protect the privacy of the subjects.
 
+## Notes
+
+- names obviously taken out
+- grouping of education
+- age groups from date of birth
+- citizenship -> other vs danish or keep it out
+- marital status married ever/never
+
 ## Suggestions
 
 ## Things to implement - implementer
--- src/anonymize.R
+++ src/anonymize.R
@@ -1,1 +1,35 @@
 library("sdcMicro")
+library("shiny")
+
+# TODO: set your workign directory to repo root
+setwd('~/git/SecurityPrivacy')
+
+# for visual exploration
+
+#sdc <- sdcApp()
+
+# read the files
+private_data <- read.csv("data/private_dataB.csv")
+public_register_data <- read.csv("data/public_data_registerB.csv")
+public_results_data <- read.csv("data/public_data_resultsB.csv")
+
+# observe and transform variables
+names(private_data)
+head(private_data)
+
+private_data$evote <- as.factor(private_data$evote)
+private_data$zip <- as.factor(private_data$zip)
+
+private_data$dob <- as.Date(private_data$dob)
+
+private_data$age <- 2020 - as.numeric(format(private_data$dob, "%Y"))
+
+min(private_data$age) ## 19 so it must be recent
+hist(private_data$age) ## normally distributed around 40-45
+
+# create sdcObject
+
+private_sdc <- createSdcObj(private_data, keyVars = c("name", "sex", 
+                                                      "evote", "citizenship",
+                                                      "zip", "age", "marital_status",
+                                                      "education"), sensibleVar = c("party"))
