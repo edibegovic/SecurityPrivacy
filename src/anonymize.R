2ef6d5789f68d0749c26ae0f7ba377a383e87508
HogniJacobsen <42565152+HogniJacobsen@users.noreply.github.com>
2020-11-05 08:29:06 +0100

    Added age groups and got real ages.
    Added arbitraty age groups and got ages for individuals using actual bday and not just year.

--- src/anonymize.R
+++ src/anonymize.R
@@ -1,8 +1,10 @@
 library("sdcMicro")
 library("shiny")
+library("eeptools")
+library("dplyr")
 
 # TODO: set your workign directory to repo root
-setwd('~/git/SecurityPrivacy')
+setwd('~/GitHub/SecurityPrivacy')
 
 # for visual exploration
 
@@ -22,10 +24,25 @@ private_data$zip <- as.factor(private_data$zip)
 
 private_data$dob <- as.Date(private_data$dob)
 
-private_data$age <- 2020 - as.numeric(format(private_data$dob, "%Y"))
+#private_data$age <- 2020 - as.numeric(format(private_data$dob, "%Y"))
+
+private_data$age <- floor(age_calc(private_data$dob, units = "years"))
 
 min(private_data$age) ## 19 so it must be recent
+max(private_data$age)
 hist(private_data$age) ## normally distributed around 40-45
+
+#Age brackets.
+private_data$age_group <- private_data$age
+private_data$age_group <- ifelse((private_data$age<20) , '<20',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>20 & private_data$age<=30) , '20-30',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>30 & private_data$age<=40) , '30-40',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>40 & private_data$age<=50) , '40-50',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>50 & private_data$age<=60) , '50-60',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>60 & private_data$age<=70) , '60-70',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>70 & private_data$age<=80) , '70-80',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>80 & private_data$age<=90) , '80-90',private_data$age_group)
+private_data$age_group <- ifelse((private_data$age>90) , '>90',private_data$age_group)
 
 # create sdcObject
 
@@ -39,3 +56,4 @@ private_sdc <- createSdcObj(private_data, keyVars = c("name", "sex",
 
 private_sdc
 
+private_data
